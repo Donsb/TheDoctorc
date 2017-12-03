1bce6b6029b353f35be461e7f2e24ba3acc29ade
Donald Belliveau <donaldsbelliveau@gmail.com>
2017-12-03 14:16:19 -0800

    Added IB Outlets for Companion page: image, name, actor & active years.  Added connections in storyboard.

---
+++ TheDoctorc/Views/CompanionCell.swift
@@ -0,0 +1,24 @@
+//
+//  CompanionCell.swift
+//  TheDoctorc
+//
+//  Created by Donald Belliveau on 2017-12-03.
+//  Copyright © 2017 Donald Belliveau. All rights reserved.
+//
+
+import UIKit
+
+class CompanionCell: UICollectionViewCell {
+    
+    /*
+     IBOutlets
+     */
+    
+    @IBOutlet weak var companionImage: UIImageView!
+    @IBOutlet weak var companionName: UILabel!
+    @IBOutlet weak var companionActor: UILabel!
+    @IBOutlet weak var companionActiveYears: UILabel!
+    
+}
+
+// 
